14587dad76f7a1a1513e0a87c13e8d0984d2783b
Alex <diamondspurs@hotmail.co.uk>
2021-07-05 18:03:01 +0100

    First draft final submission

---
+++ js/app.js
@@ -0,0 +1,5 @@
+baguetteBox.run('.gallery');
+
+const search = document.querySelector('input');
+let caption = document.querySelectorAll('a').getAttribute('data-caption');
+let picture = document.getElementsByClassName('pictures');
